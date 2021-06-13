1b8edbf9bab8364481b5c9654020c3adee89c6a9
Vaishnavi-eng137 <vaisuch@gmail.com>
2021-06-13 13:21:07 +0530

    component part-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css';
+import Menu from "./components/MenuComponent"
+
+function App () {
+    return (
+      <div className="App">
+       <nav className="navbar-dark navbar-expand-lg bg-primary">
+         <div className="container">
+         <a className="navbar-brand" href="\">Ristorante Con Fusion</a>
+         </div>
+       </nav>
+       <Menu/>
+      </div>
+    );
+  }
+  
 
 export default App;
