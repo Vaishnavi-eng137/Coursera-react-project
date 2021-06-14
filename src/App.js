aa5cf2d15cc790388b08d5e16ea164282cd91077
Vaishnavi-eng137 <vaisuch@gmail.com>
2021-06-14 12:52:57 +0530

    componentpart2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
+import "./App.css";
+import React,{Component} from "react";
+import { DISHES } from "./shared/dishes";
+import Menu from "./components/MenuComponent";
 
-import './App.css';
-import Menu from "./components/MenuComponent"
-
-function App () {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+  render() {
     return (
       <div className="App">
-       <nav className="navbar-dark navbar-expand-lg bg-primary">
-         <div className="container">
-         <a className="navbar-brand" href="\">Ristorante Con Fusion</a>
-         </div>
-       </nav>
-       <Menu/>
+        <nav className="navbar-dark navbar-expand-lg bg-primary">
+          <div className="container">
+            <a className="navbar-brand" href="\">
+              Ristorante Con Fusion
+            </a>
+          </div>
+        </nav>
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
-  
+}
 
 export default App;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,77 +4,58 @@ class Menu extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: [
-        {
-          id: 0,
-          name: "Uthappizza",
-          image: "assets/images/uthappizza.png",
-          category: "mains",
-          label: "Hot",
-          price: "4.99",
-          description:
-            "A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
-        },
-        {
-          id: 1,
-          name: "Zucchipakoda",
-          image: "assets/images/zucchipakoda.png",
-          category: "appetizer",
-          label: "",
-          price: "1.99",
-          description:
-            "Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce",
-        },
-        {
-          id: 2,
-          name: "Vadonut",
-          image: "assets/images/vadonut.png",
-          category: "appetizer",
-          label: "New",
-          price: "1.99",
-          description:
-            "A quintessential ConFusion experience, is it a vada or is it a donut?",
-        },
-        {
-          id: 3,
-          name: "ElaiCheese Cake",
-          image: "assets/images/elaicheesecake.png",
-          category: "dessert",
-          label: "",
-          price: "2.99",
-          description:
-            "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
-        },
-      ],
+      selectedDish: null,
     };
   }
-  render(){
-      const menu = this.state.dishes.map((dish)=>{
-          return(
-              <div key ={dish.id} className="col-12 mt-5">
-                  <div className="media d-flex">
-                      <img src={dish.image} className="d-flex mr-3 align-self-center " alt ={dish.name}></img>
-                      <div className="media-body">
-                       <h2 className="mt-0">{dish.name}</h2>
-                       <p>{dish.description}</p>
-                      </div>
-                  </div>
-              </div>
-          )
-      })
-      return(
-          <div className="container">
-              <div className="row">
-                  <ul className="list-unstyled">
-                  <li className="media">
-                      {menu}
-                  </li>
-                  </ul>
-                  
-              </div>
+  onDishSelect(dish) {
+    this.setState({ selectedDish: dish });
+  }
+  renderDish(dish) {
+    if (dish != null)
+      return (
+        <div className="card">
+          <img src={dish.image} alt={dish.name}></img>
+          <div className="card-body">
+            <h5 className="card-title">{dish.name}</h5>
+            <p className="card-text">{dish.description}</p>
           </div>
-      )
+        </div>
+      );
+  }
+  render() {
+    const menu = this.props.dishes.map((dish) => {
+      return (
+        <div key={dish.id} className="col-12 col-md-5 m-1">
+              <div
+                className="card"
+                onClick={() => {
+                  this.onDishSelect(dish);
+                }}
+              >
+                <img
+                  className="card-img"
+                  src={dish.image}
+                  alt={dish.name}
+                  width="100%"
+                ></img>
+                <div className="card-img-overlay">
+                  <h2 className=" card-title">{dish.name}</h2>
+                </div>
+              </div>
+            </div>
+         
+      );
+    });
+    return (
+      <div className="container">
+        <div className="row">{menu}</div>
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            {this.renderDish(this.state.selectedDish)}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
-
 export default Menu;
